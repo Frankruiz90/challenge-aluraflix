dc5a71b72f132f9a5696ea4e0fae623227c44c68
frank <frankruiz_90@hotmail.com>
2024-12-12 07:54:56 -0500

    add new components and jsonserver

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Card from "../../components/Card/Card";
 import styles from "./home.module.scss";
+import Title from "../../components/Title/Title";
 
 export default function Home() {
   const [videos, setVideos] = useState([]);
@@ -11,12 +12,29 @@ export default function Home() {
         setVideos(data);
       });
   }, []);
+  const groupedVideos = videos.reduce((acc, video) => {
+    if (!acc[video.category]) {
+      acc[video.category] = [];
+    }
+    acc[video.category].push(video);
+    return acc;
+  }, {});
 
   return (
     <div className={styles.home}>
-      {videos.map((video) => {
-        return <Card {...video} key={video.id} />;
-      })}
+      {Object.keys(groupedVideos).map((category) => (
+        <div
+          key={category}
+          className={`${styles.categoryGroup} ${styles[category] || ""}`}
+        >
+          <Title title={category} />
+          <div className={styles.cards}>
+            {groupedVideos[category].map((video) => (
+              <Card {...video} key={video.id} />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
